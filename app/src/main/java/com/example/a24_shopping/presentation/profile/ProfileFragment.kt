e95b9223cbd8750e643c954e5f4a42c07efd9649
Songmi <ksongmi0727@gmail.com>
2022-03-05 23:33:43 +0900

    0305

--- app/src/main/java/com/example/a24_shopping/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/a24_shopping/presentation/profile/ProfileFragment.kt
@@ -1,9 +1,16 @@
 package com.example.a24_shopping.presentation.profile
 
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.a24_shopping.R
 import com.example.a24_shopping.databinding.FragmentProductListBinding
+import com.example.a24_shopping.databinding.FragmentProfileBinding
 import com.example.a24_shopping.presentation.BaseFragment
+import com.example.a24_shopping.presentation.adapter.ProductListAdapter
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -17,9 +24,6 @@ internal class ProfileFragment: BaseFragment<ProfileViewModel, FragmentProductLi
 
     override fun getViewBinding(): FragmentProductListBinding = FragmentProductListBinding.inflate(layoutInflater)
 
-    override fun observeData() {
-        TODO("Not yet implemented")
-    }
 
     private val gso: GoogleSignInOptions by lazy {
         GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -32,5 +36,64 @@ internal class ProfileFragment: BaseFragment<ProfileViewModel, FragmentProductLi
 
     private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
 
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAcctountFromIntent(result.data)
+            try{
+              task.getResult(ApiException::class.java)?.let{ account ->
+                  Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                  viewModel.saveToken(account.idToken ?: throw  Exception())
+              } ?: throw Exception()
+            } catch(e: Exception){
+               e.printStackTrace()
+                handleErrorState()
+             }
+        }
+
+    }
+
+    private val adapter = ProductListAdapter()
+
+    private fun initViews(binding: FragmentProfileBinding) = with(binding){
+
+    }
+
+    private fun signInGoogle() {
+        val signInIntent: Intent = gsc.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private fun signOut() {
+        firebaseAuth.signOut()
+        viewModel.signOut()
+    }
+
+    override fun observeData() {
+        viewModel.profileStateLiveData.observe(this){
+            when(it){
+
+            }
+        }
+    }
+
+    private fun handleLoadingState() = with(binding) {
+
+    }
+
+    private fun handleLogin(state: ProfileState.Login){
+
+    }
+
+    private fun handleSuccessState(state: ProfileState.Success) = with(binding){
+
+    }
+
+    private fun handleRegisteredState(state: ProfileState.Success.Registered) = with(binding) {
+
+    }
+
+    private fun handleErrorState(){
+
+    }
 
 }
--- app/src/main/java/com/example/a24_shopping/presentation/profile/ProfileState.kt
+++ app/src/main/java/com/example/a24_shopping/presentation/profile/ProfileState.kt
@@ -1,4 +1,28 @@
 package com.example.a24_shopping.presentation.profile
 
-class ProfileState {
+import android.net.Uri
+import com.example.a24_shopping.data.entity.product.ProductEntity
+
+internal sealed class ProfileState {
+
+    object UnInitialized: ProfileState()
+
+    object Loading: ProfileState()
+
+    data class Login(
+        val idToken: String
+    ): ProfileState()
+
+    sealed class Success: ProfileState(){
+
+        data class Registered(
+            val userName: String,
+            val profileImgUri: Uri?,
+            val productList: List<ProductEntity> = listOf()
+        ): Success()
+
+        object NotRegistered: Success()
+    }
+
+    object Error: ProfileState()
 }
